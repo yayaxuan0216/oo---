5f324bb2b5d329c1745b533cb1b8f3ec875fdd4f
alexHYC25 <148223120+alexHYC25@users.noreply.github.com>
2025-12-25 11:05:37 +0800

    index.js更新

--- node-project/index.js
+++ node-project/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api', authRoutes);
 app.use('/api', contractRoutes);
 app.use('/api', userRoutes);
-app.use('/api', rentalRoutes);
+app.use('/api/rentals', rentalRoutes);
 app.use('/api', appointmentRoutes);
 
 
