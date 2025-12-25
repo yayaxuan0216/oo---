8d4b666e486590bc90477dce1a85efe401fd8ec0
alexHYC25 <148223120+alexHYC25@users.noreply.github.com>
2025-12-25 12:36:50 +0800

    調整錯誤

--- node-project/routes/user.js
+++ node-project/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const updateProfile = require('../controllers/user/updateProfile');
 const getUserById = require('../controllers/user/getUserById');
-const controller = require('../controllers/tenantsController');
+const controller = require('../controllers/contracts/tenantsController');
 router.post('/update', updateProfile); // 更新資料
 router.get('/:id', getUserById);
 router.get('/', controller.getRoomTenants);
