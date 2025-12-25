a3136f2ed60c40cf2de1ebcc41e61cd6000d1299
alexHYC25 <148223120+alexHYC25@users.noreply.github.com>
2025-12-25 11:36:27 +0800

    合約api調用方式修改

--- node-project/index.js
+++ node-project/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/auth', authRoutes);
-app.use('/api/contracts', contractRoutes);
+app.use('/api', contractRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/rentals', rentalRoutes);
 app.use('/api/appointments', appointmentRoutes);
